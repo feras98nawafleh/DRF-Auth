9aaaeb9beabcf178047950ec54f30d93efb2a2b6
feras98nawafleh <feras.a.nawafleh@gmail.com>
2021-12-14 20:18:12 +0200

    finished lab33

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/posts/', include('Post.urls')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
 ]
--- Blog/Post/views.py
+++ Blog/Post/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import PostSerialzer
+from .models import Post
+from .permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+# create your views here
+
+class PostList(generics.ListCreateAPIView):
+    # queryset = Post.objects.filter(published = True)
+    queryset = Post.objects.all()
+    serializer_class = PostSerialzer
+    permission_class = (IsAuthenticatedOrReadOnly,)
+    
+
+# RUD view
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerialzer
+    permission_class = (IsAuthenticatedOrReadOnly,)
